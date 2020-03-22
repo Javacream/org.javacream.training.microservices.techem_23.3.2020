93442746d546f6266701aa8decbeba71c4838d94
Rainer Sawitzki <training@rainer-sawitzki.de>
2020-03-22 09:15:55 +0100

    remove unnecessary projekt Spring

--- org.javacream.training.spring.books/src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-insert into STORE (category, item, stock) values ('books', 'ISBN1', 42)
-insert into STORE (category, item, stock) values ('books', 'ISBN2', 4)
-insert into STORE (category, item, stock) values ('books', 'ISBN3', 2)
-insert into STORE (category, item, stock) values ('books', 'ISBN4', 142)
